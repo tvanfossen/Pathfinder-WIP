4879f85b7a8f1faed9352e8e93e2678a7a70e30e
tvanfossen <tristan.vanfossen@yahoo.com>
2017-05-08 10:05:41 -0400

    Working version
    Updated algorithm from purely depth first to a narrowed depth search
    after implementation of djikstra's algorithm on the given matrix

--- main.cpp
+++ main.cpp
@@ -1,77 +1,43 @@
-#include <iostream>
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "pathfinder.h"
 
-using namespace std;
-
-int findPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight, int* pOutBuffer, const int nOutBufferSize);
-int pathAvailable(const unsigned char* pMap, const int nTargetX, const int nTargetY, const int nStartX, const int nStartY, const int nMapWidth, const int nMapHeight, int pathLength);
 int main()
 {
-    int nMapWidth = 4;
-    int nMapHeight = 3;
-    int nStartX = 0;
-    int nStartY = 0;
-    int nTargetX = 1;
-    int nTargetY = 2;
-    int nOutBufferSize = 12;
-    int pOutBuffer[nOutBufferSize];
-    unsigned char pMap [] = {1, 1, 1, 1,
-                             0, 1, 0, 1,
-                             0, 1, 1, 1};
+	int nMapWidth = 16;
+	int nMapHeight = 5;
+	int nStartX = 6;
+	int nStartY = 2;
+	int nTargetX = 15;
+	int nTargetY = 1;
+	unsigned char pMap[] = { 0, 0, 1, 0, 1, 1, 1, 0, 1 };
+	int pOutBuffer[7];
 
+	/* For debugging readout
+	unsigned char pMap [] = {0, 1, 2, 3,
+							 4, 5, 6, 7,
+							 8, 9, 10, 11};
+	*/
 
+	int length = FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
 
-    printf("%d\n\n",findPath(nStartX, nStartY, nTargetX, nTargetY, pMap, nMapWidth, nMapHeight, pOutBuffer, nOutBufferSize));
+	
+	printf("Minimum path length of: %d\n\n", length);
+	printf("Buffer: %d", pOutBuffer[0]);
+	for (int i = 1; i<length; i++)
+	{
+		printf(", %d", pOutBuffer[i]);
+	}
+	printf("\n\n");
+	//visualize(nStartX,nStartY,pMap,nMapWidth,nMapHeight);
+	//finVisualize(nStartX,nStartY,pMap,nMapWidth,nMapHeight,pOutBuffer,nOutBufferSize);
 
-    return 0;
+	//printf("Runtime: %lu \n", endTV.tv_sec - startTV.tv_sec);
+
+	while (true)
+	{
+
+	}
+
+	return 0;
 }
-
-int findPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight, int* pOutBuffer, const int nOutBufferSize)
-{
-
-    /*for (int i=0;i<(nMapHeight*nMapWidth);i++) //For visualization
-    {
-        printf("%u", *(pMap+i));
-        if (i%nMapWidth==nMapWidth-1)
-        {
-            printf("\n");
-        }
-    }*/
-
-
-    if (*(pMap + ((nMapHeight+1)*(nStartY+1))) == 1 && nStartY < nMapHeight && *(pOutBuffer+nOutBufferSize) != ((nMapHeight+1)*nStartY+1) + (1*(nStartX+1))) //Checks for available position down 1
-    {
-        printf("check\n");
-        *(pOutBuffer+(nOutBufferSize-1)) = (nMapHeight+1)*(nStartY+1) + (1*(nStartX+1));
-        return 1+ findPath(nStartX, nStartY+1, nTargetY, nTargetX, pMap, nMapWidth,nMapHeight, pOutBuffer,nOutBufferSize-1);
-
-    }
-    else if (*(pMap + (1*(nStartX+1))) == 1 && nStartX < nMapWidth && *(pOutBuffer+nOutBufferSize) != (1*(nStartX+1))+((nMapHeight+1)*nStartY+1))
-    {
-        printf("check2\n");
-        *(pOutBuffer+(nOutBufferSize-1)) = (1*(nStartX+1)+((nMapHeight+1)*(nStartY+1)));
-        return 1+ findPath(nStartX+1, nStartY, nTargetY, nTargetX, pMap, nMapWidth,nMapHeight, pOutBuffer,nOutBufferSize-1);
-    }
-    else if (*(pMap + ((nMapHeight+1)*(nStartY-1))) == 1 && nStartY > 0 && *(pOutBuffer+nOutBufferSize) != ((nMapHeight+1)*(nStartY-1)) + (1*(nStartX+1)))
-    {
-        printf("check3\n");
-        *(pOutBuffer+(nOutBufferSize-1)) = (nMapHeight+1)*(nStartY-1)+ (1*(nStartX+1));
-        return 1 + findPath(nStartX, nStartY-1, nTargetY, nTargetX, pMap, nMapWidth,nMapHeight, pOutBuffer,nOutBufferSize-1);
-    }
-    else if (*(pMap + (1*(nStartX-1))) == 1 && nStartX > 0 && *(pOutBuffer + nOutBufferSize) != (1*(nStartX-1)) + (nMapHeight+1)*(nStartY+1))
-    {
-        printf("check4\n");
-        *(pOutBuffer+(nOutBufferSize-1)) = (1*(nStartX-1)) + ((nMapHeight+1)+(nStartY+1));
-        return 1 + findPath(nStartX-1, nStartY, nTargetY, nTargetX, pMap, nMapWidth,nMapHeight, pOutBuffer,nOutBufferSize-1);
-    }
-    else return 0;
-}
-
-
-
-
-
-
---
+++ pathfinder.h
@@ -0,0 +1,14 @@
+#ifndef PATHFINDER_H_INCLUDED
+#define PATHFINDER_H_INCLUDED
+
+int FindPath(const int nStartX, const int nStartY,
+	const int nTargetX, const int nTargetY,
+	const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
+	int* pOutBuffer, const int nOutBufferSize); void visualize(const int nStartX, const int nStartY, const int* pMap, const int nMapWidth, const int nMapHeight);
+void visualize(const int nStartX, const int nStartY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight);
+void finVisualize(const int nStartX, const int nStartY, const unsigned char* pMap, const int nMapWidth, const int nMapHeight, const int* pOutBuffer, const int nOutBufferSize);
+void djikstra(const int nStartX, const int nStartY, const unsigned char* pMap, int* weighted, const int nMapWidth, const int nMapHeight);
+bool bufferPath(const int nStartX, const int nStartY, const int nTargetX, const int nTargetY, const unsigned char* pMap, const int* weighted, const int nMapWidth, const int nMapHeight, int* pOutBuffer, const int nOutBufferSize, const int minLength, const int bufferPos);
+
+
+#endif // PATHFINDER_H_INCLUDED
