9f19d97b707b0c753cac472bbe7abf3e0eb06244
tvanfossen <tristan.vanfossen@yahoo.com>
2017-06-27 12:06:02 -0400

    update for mass testing

--- main.cpp
+++ main.cpp
@@ -1,34 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pathfinder.h"
+#include <time.h>
 
 int main()
 {
-	int nMapWidth = 16;
-	int nMapHeight = 5;
-	int nStartX = 6;
-	int nStartY = 2;
-	int nTargetX = 15;
-	int nTargetY = 1;
-	unsigned char pMap[] = { 0, 0, 1, 0, 1, 1, 1, 0, 1 };
-	int pOutBuffer[7];
+	int pOutBuffer[100000];
+	unsigned char xMap[100000];
+	int length;
+	time_t t;
+	srand((unsigned) time(&t));
 
-	/* For debugging readout
-	unsigned char pMap [] = {0, 1, 2, 3,
-							 4, 5, 6, 7,
-							 8, 9, 10, 11};
-	*/
+	xMap[0] = 1;
 
-	int length = FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
+    for (int j = 0; j<100000; j++)
+    {
+        for (int i = 1; i<100000; i++)
+        {
+            xMap[i] = rand()%2;
+        }
 
-	
-	printf("Minimum path length of: %d\n\n", length);
+        length = FindPath(0, 0, 10, 15, xMap, 100, 100, pOutBuffer, 10000);
+
+        if (length>0)
+        {
+            printf("Minimum path length of: %d\n\n", length);
+            printf("Buffer: %d", pOutBuffer[0]);
+            for (int i = 1; i<length; i++)
+            {
+                printf(", %d", pOutBuffer[i]);
+            }
+            printf("\n\n");
+        }
+    }
+
+    printf("completed");
+
+	//int length = FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
+
+
+	/*printf("Minimum path length of: %d\n\n", length);
 	printf("Buffer: %d", pOutBuffer[0]);
 	for (int i = 1; i<length; i++)
 	{
 		printf(", %d", pOutBuffer[i]);
 	}
 	printf("\n\n");
+	*/
+
 	//visualize(nStartX,nStartY,pMap,nMapWidth,nMapHeight);
 	//finVisualize(nStartX,nStartY,pMap,nMapWidth,nMapHeight,pOutBuffer,nOutBufferSize);
 
